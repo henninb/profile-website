269b3d30c439adf5b1af79185ee88bd2aa4967ad
Brian Henning <henninb@msn.com>
2024-05-22 18:15:36 -0500

    wip

--- server.js
+++ server.js
@@ -9,27 +9,47 @@ const port = process.env.PORT || 3000
 const app = express()
 
 const pxConfig = {
-  px_app_id: 'PX_APP_ID',
-  px_cookie_secret: 'PX_COOKIE_ENCRYPTION_KEY',
-  px_auth_token: 'PX_TOKEN',
-  px_module_mode: "monitor",
-  px_bypass_monitor_header: "x-px-block",
+  px_app_id: 'PXjJ0cYtn9',
+  px_cookie_secret: '',
+  px_auth_token: '',
+  px_bypass_monitor_header: 'x-px-block',
+  px_module_mode: 'active_blocking',
+  px_logger_severity: 'debug',
   px_login_credentials_extraction_enabled: false,
 };
+
 perimeterx.init(pxConfig);
 
-/* block high scored users using px-module for all routes */
 app.use(perimeterx.middleware);
 
 app.listen(port, () => { console.log(`listening on port ${port}`) });
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-// app.use(cors());
 
-app.post('/api/login', (_, response) => {
-   response.send('api login POST called.');
+app.use(express.static('public'), (_req,_response,next) => {
+  next();
 });
 
-app.get('/helloWorld', perimeterx.middleware, (_, response) => {
-    response.send('Hello World');
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.send(error.message || 'Server internal error');
+// });
+
+// app.use(cors());
+
+app.post('/api/login', (_, response, next) => {
+  console.log('api login post');
+  response.send('api login POST called.');
+  next();
+});
+
+// app.get('/helloWorld', perimeterx.middleware, (_, response, next) => {
+//   console.log('hello world');
+//   response.send('Hello World');
+//   next();
+// });
+
+app.get('/helloWorld', (_, response, next) => {
+  console.log('hello world');
+  response.send('Hello World');
+  next();
 });
