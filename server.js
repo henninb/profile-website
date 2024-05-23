76e8d51d95a4529ba50afd31db0806b713355a8e
Brian Henning <henninb@msn.com>
2024-05-23 07:27:17 -0500

    wip

--- server.js
+++ server.js
@@ -3,8 +3,12 @@
 const express = require('express')
 const perimeterx = require('perimeterx-node-express');
 const fs = require('fs');
+const axios = require('axios');
 //const cors = require('cors');
 //const bodyParser = require('body-parser');
+const EMAIL = 'henninb@gmail.com'; // Replace with your email
+const PASSWORD = 'monday1'; // Replace with your password
+const JWT_KEY = 'your_jwt_key'; // Replace with your JWT key
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -12,15 +16,11 @@ const app = express()
 const configPath = './config.json';
 const pxConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
 
-// const pxConfig = {
-//   px_app_id: 'PXjJ0cYtn9',
-//   px_cookie_secret: '',
-//   px_auth_token: '',
-//   px_bypass_monitor_header: 'x-px-block',
-//   px_module_mode: 'active_blocking',
-//   px_logger_severity: 'debug',
-//   px_login_credentials_extraction_enabled: false,
-// };
+app.use((request, response, next) => {
+  console.log(`Request URL: ${request.url}`);
+  response.header("x-powered-by", "ExpressServer");
+  next();
+});
 
 const PxMiddlewareWrap = (req, res, next) => {
   if (pxConfig.px_filter_by_route && pxConfig.px_filter_by_route.includes(req.path)){
@@ -42,7 +42,6 @@ const setPxMiddleware = (app) => {
   });
 }
 
-
 perimeterx.init(pxConfig);
 
 var PxMiddleware;
@@ -54,19 +53,12 @@ app.use(PxMiddlewareWrap);
 
 setPxMiddleware(app);
 
-//app.use(perimeterx.middleware);
-
 app.listen(port, () => { console.log(`listening on port ${port}`) });
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'), (_req,_response,next) => {
   next();
 });
-
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500);
-//   res.send(error.message || 'Server internal error');
-// });
 
 // app.use(cors());
 
@@ -76,12 +68,19 @@ app.post('/api/login', (_, response, next) => {
   next();
 });
 
-// app.get('/helloWorld', perimeterx.middleware, (_, response, next) => {
-//   console.log('hello world');
-//   response.send('Hello World');
-//   next();
-// });
-//
+app.get('/api/nhl', (_request, response) => {
+  const url = 'https://fixturedownload.com/feed/json/nhl-2023/minnesota-wild';
+
+  axios.get(url)
+    .then(res => {
+      // If the request is successful, send the JSON response back to the client
+      response.status(200).json(res.data);
+    })
+    .catch(error => {
+      console.error('Error fetching NHL schedule:', error);
+      response.status(500).send('Error fetching data');
+    });
+});
 
 app.get('/helloWorld', (_, response, next) => {
   console.log('hello world');
